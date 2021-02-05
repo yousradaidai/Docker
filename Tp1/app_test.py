c450c7e9a2c1191c808d0f9933e63fff9b6d61d3
yousradaidai <daidaiyousra@gmail.com>
2021-02-05 14:51:58 +0100

    install python3 & pip3 & vim

---
+++ Tp1/app_test.py
@@ -0,0 +1,13 @@
+import sys, os from flask import Flask
+
+app = Flask(__name__)
+
+#route for home page
+@app.route("/")
+def index():
+    return "Hello AP Formation 🤖 ! \n\nCeci est une micro application python faite avec Flask 🐍 \n\n"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
+
